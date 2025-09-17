0917cec9d764f39c0b46c3c28cfb252ea9a2903c
surayac <surayacsm@gmail.com>
2025-09-17 15:53:49 +0200

    feat(router): add application routing setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { Toaster } from 'react-hot-toast'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import AppRoutes from './application/Router'
 
 function App() {
-
-
   return (
     <>
-     
+      <Toaster position="top-center" reverseOrder={false} />
+      <Header />
+      <AppRoutes />
+      <Footer />
     </>
   )
 }
