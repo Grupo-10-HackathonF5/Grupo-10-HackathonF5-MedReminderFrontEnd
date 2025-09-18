e6773673990f59c3ab4493920dab7598aaa97917
surayac <surayacsm@gmail.com>
2025-09-18 17:52:22 +0200

    feat(newhomepage): add new homepage with medications list

--- src/application/Router.jsx
+++ src/application/Router.jsx
@@ -2,11 +2,13 @@ import { Routes, Route } from "react-router-dom";
 import List from "../pages/list/List";
 import Calendar from "../components/calendar/Calendar";
 import Create from "../pages/create/Create";
+import Homepage from "../pages/homepage/Homepage";
 
 export default function AppRoutes() {
   return (
     <Routes>
-      <Route path="/" element={<List />} />
+      <Route path="/" element={<Homepage />} />
+      <Route path="/list" element={<List />} />
       <Route path="/calendar" element={<Calendar />} />
       <Route path="/create" element={<Create />} />
     </Routes>
