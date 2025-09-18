18a4ac45771639407ae3c90bec2f6e982b0b6fef
Fer <desarrollowebfer@gmail.com>
2025-09-18 18:38:44 +0000

    feat: add and modified files tests

--- eslint.config.js
+++ eslint.config.js
@@ -15,7 +15,12 @@ export default defineConfig([
     ],
     languageOptions: {
       ecmaVersion: 2020,
-      globals: globals.browser,
+      globals: {
+        ...globals.browser,
+        test: 'readonly',
+        expect: 'readonly',
+        jest: 'readonly'
+      },
       parserOptions: {
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
