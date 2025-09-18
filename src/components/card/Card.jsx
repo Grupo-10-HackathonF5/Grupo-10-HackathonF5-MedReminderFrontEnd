def3b28552490b1da7b9577ca09a110824fb488a
surayac <surayacsm@gmail.com>
2025-09-18 09:35:46 +0200

    fix(card): correcting validation of medicament taken

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,7 +1,17 @@
-import "./Card.css"
+import React, { useState } from "react";
+import toast from "react-hot-toast";
+import "./Card.css";
+
 const Card = () => {
+    const [taken, setTaken] = useState(false);
 
-
+    const toggleTaken = () => {
+        if (!taken) {
+            toast.success("Has tomado tu medicamento");
+        }
+        setTaken(!taken);
+    };
+    
     return (
         <main className="main">
             <section className="greetingSection">
@@ -12,9 +22,12 @@ const Card = () => {
 
             <section className="cardsSection">
                 <div className="card">
-                    <h2 className="cardTitle">Medicamento</h2>
-                    <p className="cardDosage">500mg</p>
+                    <h2 className="cardTitle">Medicamento  500mg</h2>
+                    <button className={`checkButton ${taken ? 'checked' : ' '}`}
+                        onClick={toggleTaken}>{taken && "✔"}</button>
                 </div>
+
+
             </section>
         </main>
     );
